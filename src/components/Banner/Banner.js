19d5fe0c1e479a66e0dbadcbc82757fe90dcff97
Suresh Godakanda <wenukagtx@gmail.com>
2024-08-24 14:51:31 +0530

    banner done

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import Link from 'next/link';
 import './banner.scss';
+import background from '../../../public/banner-1.jpg';
 
 export default function Banner() {
     return (
-        <section className="banner">
-            Banner
+        <section className="banner" style={{ backgroundImage: `url(${background.src})` }}>
+            <div className="container">
+                <h1>Where design dreams come to life</h1>
+                <Link href="/contact" className="btn btn-primary">Get in touch</Link>
+            </div>
         </section>
     )
 }
