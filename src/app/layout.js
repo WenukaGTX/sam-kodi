ba19e0f82e1d993c564b777b1fab4fc65c1a058c
Suresh Godakanda <wenukagtx@gmail.com>
2024-08-26 23:52:08 +0530

    default theme

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider attribute="class">
+        <ThemeProvider attribute="class" defaultTheme="dark">
           <Navbar />
           {children}
           <Footer />
