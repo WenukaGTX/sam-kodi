94c4d91954b6c2d874eafa267d1fac5fcc230c50
Suresh Godakanda <wenukagtx@gmail.com>
2024-08-31 14:25:44 +0530

    navbar done

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,15 +7,7 @@ export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg bg-transparent">
             <div className="container">
-                <Link href="/" className="navbar-brand">
-                    <Image
-                        src="/la7-logo.jpg"
-                        alt="LA7 Logo"
-                        width={74}
-                        height={50}
-                        priority
-                    />
-                </Link>
+                <Link href="/" className="navbar-brand">SVK</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -26,23 +18,26 @@ export default function Navbar() {
                             <Link href="/" className="nav-link active" aria-current="page">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <Link href="/about" className="nav-link">About us</Link>
+                            <Link href="/about" className="nav-link">About</Link>
                         </li>
                         <li className="nav-item">
                             <Link href="/services" className="nav-link">Services</Link>
                         </li>
                         <li className="nav-item">
-                            <Link href="/projects" className="nav-link">Projects</Link>
+                            <Link href="/case-studies" className="nav-link">Case Studies</Link>
                         </li>
                         <li className="nav-item">
                             <Link href="/blog" className="nav-link">Blog</Link>
                         </li>
                         <li className="nav-item">
+                            <Link href="/faq" className="nav-link">FAQs</Link>
+                        </li>
+                        <li className="nav-item">
                             <Link href="/contact" className="nav-link">Contact</Link>
                         </li>
-                        {/* <li className="nav-item text-center">
+                        <li className="nav-item text-center">
                             <Link href="/contact" className="btn btn-primary">Get in touch</Link>
-                        </li> */}
+                        </li>
                         {/* <li className="nav-item">
                             <ThemeToggle />
                         </li> */}
