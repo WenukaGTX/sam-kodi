27452b7ca3e326691f7b3d26d0e55977669a5422
Suresh Godakanda <wenukagtx@gmail.com>
2024-08-31 18:36:01 +0530

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,33 +6,26 @@ export default function Footer() {
     return (
         <footer className="footer bg-dark text-white">
             <div className="container">
-                <h4 className="footer-title">
-                    <Image
-                        src="/la7-logo.jpg"
-                        alt="LA7 Logo"
-                        width={47}
-                        height={32}
-                        priority
-                    />
-                </h4>
+                <h4 className="footer-title">SVK</h4>
                 <div className="row">
                     <div className="col-md-3">
                         <ul>
                             <li>Links</li>
                             <li><Link href="/">Home</Link></li>
-                            <li><Link href="/about">About us</Link></li>
+                            <li><Link href="/about">About</Link></li>
                             <li><Link href="/services">Services</Link></li>
-                            <li><Link href="/projects">Projects</Link></li>
+                            <li><Link href="/case-studies">Case Studeis</Link></li>
                             <li><Link href="/blog">Blog</Link></li>
+                            <li><Link href="/faq">FAQs</Link></li>
                             <li><Link href="/contact">Contact</Link></li>
                         </ul>
                     </div>
                     <div className="col-md-3">
                         <ul>
                             <li>Company</li>
-                            <li><Link href="/about">Who we are</Link></li>
                             <li><Link href="/press">Press</Link></li>
                             <li><Link href="/careers">Careers</Link></li>
+                            <li><Link href="/careers">Brand Use</Link></li>
                         </ul>
                     </div>
                     <div className="col-md-3">
@@ -53,7 +46,7 @@ export default function Footer() {
                     </div>
                 </div>
                 <div className="footer-bottom">
-                    <p className="text-center"><b>Made in Sri Lanka</b> • Copyright © 2024 LA7 Interior Studio. All rights reserved
+                    <p className="text-center"><b>Made in Sri Lanka</b> • Copyright © 2024 SVK. All rights reserved
                     </p>
                 </div>
             </div>
