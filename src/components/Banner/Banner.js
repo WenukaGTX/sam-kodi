d46f48a72305a60022c08adcc8d3fd7c4356bb3c
Suresh Godakanda <wenukagtx@gmail.com>
2024-08-31 16:52:28 +0530

    banner done

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -7,8 +7,19 @@ export default function Banner() {
     return (
         <section className="banner" style={{ backgroundImage: `url(${background.src})` }}>
             <div className="container">
-                <h1>Where design dreams <br className="d-none d-sm-block"/>come to life</h1>
-                <Link href="/contact" className="btn btn-primary btn-lg">Get in touch</Link>
+                <Image
+                    src="/sam-kodi-portrait-1.png"
+                    alt="Banner avatar"
+                    width={313}
+                    height={450}
+                    priority
+                    className="banner-image"
+                />
+                <div class="banner-right">
+                    <h1>Transform Your Business with Fractional CRO Expertise Tailored for Success</h1>
+                    <p>Driving Growth and Refining Business Practices <br className="d-none d-sm-block"/>Through Advanced Sales Approaches</p>
+                    <Link href="/contact" className="btn btn-primary btn-lg">Get in touch</Link>
+                </div>
             </div>
         </section>
     )
