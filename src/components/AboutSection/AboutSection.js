620d9c38f729eea8ebba51f5849f22818bc39f16
Suresh Godakanda <wenukagtx@gmail.com>
2024-09-13 16:14:40 +0530

    about title

--- src/components/AboutSection/AboutSection.js
+++ src/components/AboutSection/AboutSection.js
@@ -5,8 +5,8 @@ export default function AboutSection() {
     return (
         <section className="page-section services bg-white-x">
             <div className='container text-center'>
-                <div className="page-header mb-4">
-                    <h1>Meet Your Strategic Partner</h1>
+                <div className="page-header mb-4 w-75 mx-auto">
+                    <h1>Transform Your Business with Fractional CRO Services Tailored for Sucess</h1>
                 </div>
                 <p className='text-center w-75 mx-auto lh-lg para-text'>I'm Sameera Viraj Kodituwakku, an experienced sales leader specializing in elevating tech companies to new heights. With over a decade of leadership in sales and business development, I empower Tech companies to boost their sales, expand their revenue streams, and surpass their current achievements. My expertise extends across SaaS, MSP, and Technical Services domains, ensuring that your business receives comprehensive and customized support designed to drive significant growth.</p>
                 <Link href="https://calendly.com/sameeraviraj/30min?month=2024-09" className="btn btn-primary btn-lg mt-4" rel="noopener noreferrer" target="_blank">Schedule a call</Link>
